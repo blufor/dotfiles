24faf6b7a8edd630173c0b2eb5b364f01a30879a
Radek 'blufor' Slavicinsky <radek@blufor.cz>
2015-08-21 15:58:02 +0200

    added tools for repo sync & color modifications

--- bash/workstation.sh
+++ bash/workstation.sh
@@ -1,6 +1,8 @@
 export TERM="rxvt-256color"
 export LC_ALL="en_US.UTF-8"
 export LANGUAGE="${LC_ALL}"
+export DEVDIR="${HOME}/devel"
+
 
 [[ "${OS}" = "Linux" ]] && export VAGRANT_DEFAULT_PROVIDER=libvirt
 [[ -e ~/.gnupg/gpg-agent-info ]] && source ~/.gnupg/gpg-agent-info
